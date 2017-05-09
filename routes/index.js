81a1c64299642f733a5a6eba2722a0b0635517a5
blakeparkinson <wizardplow@gmail.com>
2017-05-08 21:56:23 -0600

    trying it this way

--- routes/index.js
+++ routes/index.js
@@ -11,18 +11,18 @@ router.get('/', function(req, res, next) {
 router.post('/photo', (req, res) => {
   AWS.config.update({ accessKeyId: process.env.awsKey, secretAccessKey: process.env.awsSecret });
 
-  var s3 = new AWS.S3();
-  s3.client.putObject({
-      Bucket: 'parkcurity',
+  var s3Bucket = new AWS.S3( { params: {Bucket: 'parkcurity'} } );
+  s3Bucket.putObject({
       Key: 'image.jpg',
-      Body: req.body.image,
-      ACL: 'public-read'
-    },function (resp) {
-      console.log(resp);
-      console.log('Successfully uploaded package.');
-    }, function(error){
-      console.log(error);
-    });
+      Body: req.body.image
+    },function (err, data) {
+      if (err){
+        console.log('Error uploading data: ', data);
+      }
+      else{
+        console.log('Successfully uploaded package.');
+      }
+    })
 
     res.json({success: true, result: 'photo received'});
 
