65c7eb098b59e0d4629308e42446d4085e241ef1
blakeparkinson <wizardplow@gmail.com>
2017-05-08 21:51:27 -0600

    s3 stuff

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var AWS = require('aws-sdk');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,7 +9,20 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/photo', (req, res) => {
-  console.log(req.body);
+  AWS.config.update({ accessKeyId: process.env.awsKey, secretAccessKey: process.env.awsSecret });
+
+  var s3 = new AWS.S3();
+  s3.client.putObject({
+      Bucket: 'parkcurity',
+      Key: 'image.jpg',
+      Body: req.body.image,
+      ACL: 'public-read'
+    },function (resp) {
+      console.log(resp);
+      console.log('Successfully uploaded package.');
+    }, function(error){
+      console.log(error);
+    });
 
     res.json({success: true, result: 'photo received'});
 
