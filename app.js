2fa368f5ce789162db92ae1ba7bef01081e5de86
blakeparkinson <wizardplow@gmail.com>
2017-05-09 19:44:52 -0600

    adding mongoose

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var mongoose = require( 'mongoose' );
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
