ec1aabc16891e6579cfdc7764786ba14d6f82c3c
Blake Parkinson <wizardplow@gmail.com>
2018-12-04 00:33:26 -0700

    cors

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 
 var app = express();
-var mongoose = require( 'mongoose' );
+var mongoose = require('mongoose');
 
-if (process.env.isProd){
+if (process.env.isProd) {
   require('newrelic');
   mongoose.connect(process.env.MONGODB_URI);
 }
-else{
+else {
   mongoose.connect('localhost:27017/parkcurity');
 }
 
@@ -30,18 +30,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 app.use('/', index);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
