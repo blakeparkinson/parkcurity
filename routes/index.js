6e1987062168fa425738f5b1ecc1cf44d97a719a
Blake Parkinson <wizardplow@gmail.com>
2019-03-27 20:15:28 -0600

    vision

--- routes/index.js
+++ routes/index.js
@@ -67,6 +67,19 @@ function authenticate(req, res, next) {
 
 }
 
+router.get('/vision', (req, res) => {
+  const vision = require('@google-cloud/vision');
+
+  // Creates a client
+  const client = new vision.ImageAnnotatorClient();
+
+  // Performs label detection on the image file
+  const [result] = await client.labelDetection('./resources/wakeupcat.jpg');
+  const labels = result.labelAnnotations;
+  console.log('Labels:');
+  labels.forEach(label => console.log(label.description));
+  res.json(labels)
+})
 
 
 router.get('/photo', (req, res) => {
